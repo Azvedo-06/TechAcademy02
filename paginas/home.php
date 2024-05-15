828bb55c89f76dea1ec754c6df9ee1cbf5cbf5b9
Azvedo-06 <azinho006@gmail.com>
2024-05-14 22:05:59 -0300

    php

--- paginas/home.php
+++ paginas/home.php
@@ -1,9 +1,7 @@
-<html>
- <link rel="stylesheet" href="css/style.css">
     <div class="container">
         <h2> Jogos em destaque </h2>
         <div class="center">
         <h1>vai ter um carrossel aqui</h1>
     </div>
-   </div>
-</html>
+
+
