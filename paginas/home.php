51f7916e24fa0c3ce778edc723557bda4cec4772
Azvedo-06 <azinho006@gmail.com>
2024-05-27 23:59:09 -0300

    adicionando o carrossel de img

--- paginas/home.php
+++ paginas/home.php
@@ -1,7 +1,34 @@
     <div class="container">
         <h2> JOGOS EM DESTAQUE </h2>
-        <div class="center">
-        <h3>vai ter um carrossel aqui</h3>
     </div>
 
-
+    <div id="carouselExampleIndicators" class="carousel slide p-3">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="imagens/fundo.jpg" class="d-block w-100 rounded" alt="dangeon-survival">
+            </div>
+            <div class="carousel-item">
+                <img src="..." class="d-block w-100 " alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
