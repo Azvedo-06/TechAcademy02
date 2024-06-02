2dd92d67c3588b28a37d3afdd7224fe9453a6fc3
Azvedo-06 <azinho006@gmail.com>
2024-06-02 20:43:09 -0300

    paginas de cada jogo mobile, adicão do primeiro jogo

--- paginas/dangeon.php
+++ paginas/dangeon.php
@@ -1,3 +1,51 @@
-<div class="container">
-    <h2><strong>Dangeon Survival</strong></h2>
+
+<div class="jogo-paginas-moba">
+    <img src="imagens/fundo.jpg" alt="dangeon" title="dangeon survival" class="img-paginas">
+
+    <h2 class="h2-paginas">DANGEON SURVIVAL</h2>
+    <p class="p-paginas" style="color: #8A97A0;">
+        Criado Por: <a href="equipe"><strong>ANDRE</strong></a> 
+        <br>
+        Engine: <a href="https://www.construct.net/en" target="_blank"><strong>Construct 3</strong></a>
+    </p>
+    <p class="p-paginas">
+        Um jogo de sobrevivencia casual, Top-Down, off-line, RPG onde temos que sobreviver a hondas de monstros,com armas e poderes magicos e sobreviver ao maximo. <br>
+        Plataforma de computadores windows, linux e macOS, com mecânicas top-down usando o Mouse e o Teclado.
+    </p>
+    
+    <h2 class="h2-paginas" style="margin: 15px; color:#8A97A0;">IMAGENS</h2>
+    <div class="center">
+        <div id="carouselExampleIndicators" class="carousel slide p-3">
+            <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="imagens-jogo/img-dangeon (2).png" class="d-block w-100 rounded" alt="dangeon-survival">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagens-jogo/img-dangeon.png" class="d-block w-100 " alt="#">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagens-jogo/img-dangeon (3).png" class="d-block w-100" alt="#">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagens-jogo/img-dangeon (4).png" class="d-block w-100" alt="#">
+                    </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <div class="jogar">
+        <p class="p-paginas" style="font-size:medium;">JOGAR DANGEON SURVIVAL</p>
+        <a href="games/jogo/index.html" target="_blank" class="botao">JOGAR AGORA</a>
+    </div>
+</div>
+
+<div class="jogo-paginas-desk">
+    <h2>aaa</h2>
 </div>
--- paginas/equipe.php
+++ paginas/equipe.php
@@ -6,21 +6,25 @@
         <div class="flex-col">
             <img src="imagens/img-andre.jpeg" alt="img-dev">
             <h2>André</h2>
+            <p><a href="https://github.com/Azvedo-06" target="_blank">GitHub:Azvedo-06</a></p>
             <p>Jogo - Dangeon Survival</p>
         </div>
         <div class="flex-col">
             <img src="imagens/img-marcos.jpg" alt="img-dev">
             <h2>Marcos</h2>
+            <p><a href="https://github.com/Marcosvini0001" target="_blank">GitHub:Marcosvini0001</a></p>
             <p>Jogo - Swampp Challenge</p>
         </div>
         <div class="flex-col">
             <img src="imagens/img-guilherme.jpg" alt="img-dev">
             <h2>Guilherme</h2>
+            <p><a href="https://github.com/#" target="_blank">GitHub:Guilherme</a></p>
             <p>Jogo - Galactic Annihilation</p>
         </div>
         <div class="flex-col">
             <img src="imagens/img-felipe.jpeg" alt="img-dev">
             <h2>Felipe</h2>
+            <p><a href="https://github.com/FelipeTr00" target="_blank">GitHub:FelipeTr00</a></p>
             <p>Jogo - Flappy Burnes</p>
         </div>
     </div>
